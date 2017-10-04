b764b332f53db9beaf40a3ff209a9b062002f555
aQziany <alin.balla@gmail.com>
2017-10-04 18:33:30 +0300

    19 transculde and replace

--- app/app.js
+++ app/app.js
@@ -28,6 +28,8 @@ myNinjaApp.directive('randomNinja', [function(){
     },
     // template: '<img ng-src="{{ninjas[random].thumb}}"/>',
     templateUrl: 'views/random.html',
+    transclude: true,
+    replace: true,
     controller: function($scope){
       $scope.random = Math.floor(Math.random() * 4);
     }
