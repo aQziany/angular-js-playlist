461f530c3b155eddc21250c7e47d4560a8acef8e
aQziany <alin.balla@gmail.com>
2017-10-04 13:43:25 +0300

    10 filters

--- app/app.js
+++ app/app.js
@@ -5,8 +5,31 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 myNinjaApp.controller('NinjaController', ['$scope' , function($scope){
 // protect scope for minify code
 
-  $scope.message = "hey y'all";
-  $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
+  // $scope.message = "hey y'all";
+  // $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
 
+  // array of objects
+  $scope.ninjas = [
+    {
+      name: "Yoshi",
+      belt: "green",
+      rate: 50
+    },
+    {
+      name: "Crystal",
+      belt: "yellow",
+      rate: 30
+    },
+    {
+      name: "Ryu",
+      belt: "orange",
+      rate: 10
+    },
+    {
+      name: "Shaun",
+      belt: "black",
+      rate: 1000
+    }
+  ];
 
 }]);
