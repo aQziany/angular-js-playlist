73c2c3212968c532680913632cc08d13c96eaeee
aQziany <alin.balla@gmail.com>
2017-10-04 13:10:07 +0300

    part 8, modules

---
+++ app/app.js
@@ -0,0 +1,20 @@
+// module name
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+
+// before your app runs
+myNinjaApp.config(function(){
+
+});
+
+
+// as your app runs
+myNinjaApp.run(function(){
+
+});
+
+
+
+.myNinjaApp.controller(function(){
+
+});
